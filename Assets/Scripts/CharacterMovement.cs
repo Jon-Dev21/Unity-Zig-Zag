22fd3b74e1333ae30d6c10f5fa1421291683dabd
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-18 03:29:31 -0400

    Lowered running speed / Attempting to fix audio distortion when rings are picked

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -56,7 +56,7 @@ public class CharacterMovement : MonoBehaviour
         }
 
         // Move the player by a certain amount forward depending on how much time has passed.
-        rb.transform.position = transform.position + transform.forward * 5 * Time.deltaTime;
+        rb.transform.position = transform.position + transform.forward * 4 * Time.deltaTime;
     }
 
     // Update is called once per frame
@@ -143,7 +143,12 @@ public class CharacterMovement : MonoBehaviour
             Destroy(other.gameObject);
 
             // Play the ring sound when picked
-            ringSound.Play();
+            if (!ringSound.isPlaying)
+                ringSound.Play();
+            else
+                ringSound.Stop();
+
+            //ringSound.PlayOneShot(ringSound.clip);
         }
     }
 }
