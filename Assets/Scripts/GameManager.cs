97826db61480e808fa58f0f04b434aa1784059de
Jon-Dev21 <jonacevedev96@gmail.com>
2021-11-17 23:34:14 -0400

    Multiple Updates to GameManager script
    Added EndGame method which reloads the game's main scene.
    
    Added new properties:
    Score, used to keep track of the player score.
    
    ScoreText, Used to grab the scoretext object in order to update it with the current score.
    
    Added method IncreaseScore which increments the score var by 1.
    
    Modified update method to update the passed text object with the score.
    
    Imported UnityEngine.UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// This script controls our game states.
@@ -10,12 +12,34 @@ public class GameManager : MonoBehaviour
     // Boolean value to tell if the game is started or not.
     public bool gameStarted;
 
+    // Variable used to store the game score.
+    public int score;
+
+    public Text scoreText;
+
     /// <summary>
     /// Sets gameStarted to true
     /// </summary>
     public void StartGame()
     {
         gameStarted = true;
+    }
+
+    /// <summary>
+    /// This method reloads the scene.
+    /// </summary>
+    public void EndGame()
+    {
+        // Load the main scene again.
+        SceneManager.LoadScene(0);
+    }
+
+    /// <summary>
+    /// This method increases the score by 1.
+    /// </summary>
+    public void IncreaseScore()
+    {
+        score++;
     }
 
     /// <summary>
@@ -29,5 +53,6 @@ public class GameManager : MonoBehaviour
         {
             StartGame();
         }
+        scoreText.text = "Score: "+score.ToString();
     }
 }
